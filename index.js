de551cfdff99e7fa35900496c2dc405a22775080
Boyko Dmytro <thenorthamigo@gmail.com>
2023-02-12 23:37:27 +0200

    added slider

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+// Burger menu
 var toggleButton = document.querySelector('.toggle-menu');
 var navBar = document.querySelector('.nav-bar');
 toggleButton.addEventListener('click', function () {
 	navBar.classList.toggle('toggle');
 });
+
+// Card slider
+var swiper = new Swiper(".slide-content", {
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        220: {
+            slidesPerView: 2,
+        },
+        440: {
+            slidesPerView: 3,
+        },
+        660: {
+          slidesPerView: 4,
+        },
+        880: {
+            slidesPerView: 5,
+        },
+      },
+  });
